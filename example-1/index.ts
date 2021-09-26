a71c3dfec09e860c06ee55c476671c612d7e5596
kunal <kunaalpardeshi77@gmail.com>
2021-09-26 18:13:23 +0530

    Indentation correction

--- example-1/index.ts
+++ example-1/index.ts
@@ -2,10 +2,10 @@ const no1 = document.getElementById("no1")! as HTMLInputElement;
 const no2 = document.getElementById("no2")! as HTMLInputElement;
 const btn = document.getElementsByTagName("button")[0];
 
-const add = (a: number, b: number ) => {   
-    console.log(a + b);
+const add = (a: number, b: number) => {
+  console.log(a + b);
 };
 
 btn.addEventListener("click", () => {
-    add(+no1.value, +no2.value);
+  add(+no1.value, +no2.value);
 });
