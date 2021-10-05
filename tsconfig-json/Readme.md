e149452e7e68d8b89f9a27bd2018a17352d649d2
kunal <kunaalpardeshi77@gmail.com>
2021-10-06 00:02:06 +0530

    compiling the entire project-multiple files

---
+++ tsconfig-json/Readme.md
@@ -0,0 +1,3 @@
+# 1. tsc --init => to generate the tsconfig.json file.
+
+# 2. add build command to package.json => tsc -w.
---
+++ tsconfig-json/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a_1 = require("./a");
+var b_1 = require("./b");
+console.log(a_1.a + b_1.b);
